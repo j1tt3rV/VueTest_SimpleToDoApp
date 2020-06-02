d61fceb388689cdacc61d26a1c34cd95e726a831
cr-jv76 <coderjourneyjv@gmail.com>
2020-06-02 00:52:15 -0600

    master

--- src/App.vue
+++ src/App.vue
@@ -1,78 +1,18 @@
 <template>
   <div id="app">
     <Header />
-    <AddTodo v-on:add-todo="addTodo" />
-    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+    <router-view />
   </div>
 </template>
 
 <script>
 import Header from "@/components/layout/Header";
-import Todos from "./components/Todos.vue";
-import AddTodo from "@/components/AddTodo";
-
 export default {
-  name: "App",
-
+  name: "app",
   components: {
     Header,
-    Todos,
-    AddTodo,
-  },
-  data() {
-    /*store todo data*/
-    return {
-      todos: [
-        {
-          id: 1,
-          title: "Todo One",
-          completed: false,
-        },
-        {
-          id: 2,
-          title: "Todo Two",
-          completed: false,
-        },
-        {
-          id: 3,
-          title: "Todo three",
-          completed: false,
-        },
-      ],
-    };
-  },
-  methods: {
-    deleteTodo(id) {
-      this.todos = this.todos.filter((todo) => todo.id !== id);
-    },
-    addTodo(newTodo) {
-      this.todos = [...this.todos, newTodo];
-    },
   },
 };
 </script>
 
-<style>
-/*global styles*/
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-  line-height: 1.4;
-}
-.btn {
-  display: inline-block;
-  border: none;
-  background: #555;
-  color: #ffffff;
-  padding: 7px 20px;
-  cursor: pointer;
-}
-.btn:hover {
-  background: #666;
-}
-</style>
+<style></style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="about">
+    <h1>About:</h1>
+    <p>This is a simple to do app made in vue, to learn new things.</p>
+  </div>
+</template>
