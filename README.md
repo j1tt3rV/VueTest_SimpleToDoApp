5acdd79ff625aef513aaf2d4065d4f25eef2eae7
cr-jv76 <coderjourneyjv@gmail.com>
2020-06-01 20:16:13 -0600

    master

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# simpletodo-vuetutorial
+# This simple app was made with a tutorial I followed on Youtube, just to learn Vue.
 
 ## Project setup
 ```
---
+++ src/components/Todos.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div v-for="todo in todos" v-bind:key="todo.id">
+      <TodoItem />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "@/components/TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"],
+};
+</script>
